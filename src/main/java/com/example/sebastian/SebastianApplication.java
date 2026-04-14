c2854f0281c439643f762406abb07ee80c669f5e
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-04-14 13:12:36 -0400

    inicio

---
+++ src/main/java/com/example/sebastian/SebastianApplication.java
@@ -0,0 +1,13 @@
+package com.example.sebastian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebastianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebastianApplication.class, args);
+	}
+
+}
